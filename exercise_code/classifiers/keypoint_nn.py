64422ca154bd35363abfa8ccb944bd167f3d9078
free-bit <erkin.turkoz@metu.edu.tr>
2020-01-05 17:52:36 +0100

    Batch norm layers added to KeypointModel

--- exercise_code/classifiers/keypoint_nn.py
+++ exercise_code/classifiers/keypoint_nn.py
@@ -27,27 +27,29 @@ class KeypointModel(nn.Module):
         # - For pool layers using stride=kernel size (2), which is by default.
         # - For fc layers using bias=True, which is by default.
         #######################################################################
-
-        # TODO: Try with batch-norm
       
         conv1 = nn.Conv2d(1, 32, kernel_size=4)    # 1x96x96 -> 32x93x93
         active1 = nn.ELU()
         pool1 = nn.MaxPool2d(2)                    # 32x93x93 -> 32x46x46
+        batch1 = nn.BatchNorm2d(32)
         drop1 = nn.Dropout(0.1)
         
         conv2 = nn.Conv2d(32, 64, kernel_size=3)   # 32x46x46 -> 64x44x44
         active2 = nn.ELU()
         pool2 = nn.MaxPool2d(2)                    # 64x44x44 -> 64x22x22
+        batch2 = nn.BatchNorm2d(64)
         drop2 = nn.Dropout(0.2)
         
         conv3 = nn.Conv2d(64, 128, kernel_size=2)  # 64x22x22 -> 128x21x21
         active3 = nn.ELU()
         pool3 = nn.MaxPool2d(2)                    # 128x21x21 -> 128x10x10
+        batch3 = nn.BatchNorm2d(128)
         drop3 = nn.Dropout(0.3)
         
         conv4 = nn.Conv2d(128, 256, kernel_size=1) # 128x10x10 -> 256x10x10
         active4 = nn.ELU()
         pool4 = nn.MaxPool2d(2)                    # 256x10x10 -> 256x5x5
+        batch4 = nn.BatchNorm2d(256)
         drop4 = nn.Dropout(0.4)
 
         flat = nn.Flatten()                        # 256x5x5 -> 6400
@@ -63,10 +65,10 @@ class KeypointModel(nn.Module):
         fc3 = nn.Linear(1000, 30)                  # 1000 -> 30
 
         self.layers = nn.Sequential(
-            conv1, active1, pool1, drop1,
-            conv2, active2, pool2, drop2,
-            conv3, active3, pool3, drop3,
-            conv4, active4, pool4, drop4,
+            conv1, active1, pool1, batch1, drop1,
+            conv2, active2, pool2, batch2, drop2,
+            conv3, active3, pool3, batch3, drop3,
+            conv4, active4, pool4, batch4, drop4,
             flat,
             fc1, active5, drop5,
             fc2, active6, drop6,
