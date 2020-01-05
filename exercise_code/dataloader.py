5beb6212119c0a4b647c1c4d33a75abacc20fe1b
free-bit <erkin.turkoz@metu.edu.tr>
2020-01-05 12:04:55 +0100

    Customized dataloader implemented

--- exercise_code/dataloader.py
+++ exercise_code/dataloader.py
@@ -2,6 +2,8 @@ from torch.utils.data import Dataset
 import pandas as pd
 import numpy as np
 
+from exercise_code.data_utils import get_keypoints, get_image
+from exercise_code.transforms import Normalize, ToTensor
 
 class FacialKeypointsDataset(Dataset):
     """Face Landmarks dataset."""
@@ -22,11 +24,11 @@ class FacialKeypointsDataset(Dataset):
 
     def __len__(self):
         #######################################################################
-        # TODO:                                                               #
+        # DONE:                                                               #
         # Return the length of the dataset                                    #
         #######################################################################
 
-        pass
+        return self.key_pts_frame.shape[0]
 
         #######################################################################
         #                             END OF YOUR CODE                        #
@@ -35,14 +37,21 @@ class FacialKeypointsDataset(Dataset):
     def __getitem__(self, idx):
         sample = {'image': None, 'keypoints': None}
         #######################################################################
-        # TODO:                                                               #
+        # DONE:                                                               #
         # Return the idx sample in the Dataset. A sample should be a          #
         # dictionary where the key, value should be like                      #
         #        {'image': image of shape [C, H, W],                          #
         #         'keypoints': keypoints of shape [num_keypoints, 2]}         #
         #######################################################################
 
+        df = self.key_pts_frame
+        sample['image'] = get_image(idx, df)[np.newaxis, ...]
+        sample['keypoints'] = get_keypoints(idx, df)
 
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample
 
         #######################################################################
         #                           END OF YOUR CODE                          #
