61beaadc4f8f320137fab55fc33e3493feefa486
free-bit <erkin.turkoz@metu.edu.tr>
2020-01-05 16:51:07 +0100

    KeypointModel class implemented

--- exercise_code/classifiers/keypoint_nn.py
+++ exercise_code/classifiers/keypoint_nn.py
@@ -9,7 +9,7 @@ class KeypointModel(nn.Module):
         super(KeypointModel, self).__init__()
 
         #######################################################################
-        # TODO: Define all the layers of this CNN, the only requirements are: #
+        # DONE: Define all the layers of this CNN, the only requirements are: #
         # 1. This network takes in a square (same width and height),          #
         #    grayscale image as input.                                        #
         # 2. It ends with a linear layer that represents the keypoints.       #
@@ -21,20 +21,72 @@ class KeypointModel(nn.Module):
         # and other layers (such as dropout or  batch normalization) to avoid #
         # overfitting.                                                        #
         #######################################################################
+        # NOTE:
+        # - For conv layers other parameters are as follows: stride=1,
+        # padding=0, dilation=1, bias=True, which are by default.
+        # - For pool layers using stride=kernel size (2), which is by default.
+        # - For fc layers using bias=True, which is by default.
+        #######################################################################
 
+        # TODO: Try with batch-norm
+      
+        conv1 = nn.Conv2d(1, 32, kernel_size=4)    # 1x96x96 -> 32x93x93
+        active1 = nn.ELU()
+        pool1 = nn.MaxPool2d(2)                    # 32x93x93 -> 32x46x46
+        drop1 = nn.Dropout(0.1)
+        
+        conv2 = nn.Conv2d(32, 64, kernel_size=3)   # 32x46x46 -> 64x44x44
+        active2 = nn.ELU()
+        pool2 = nn.MaxPool2d(2)                    # 64x44x44 -> 64x22x22
+        drop2 = nn.Dropout(0.2)
+        
+        conv3 = nn.Conv2d(64, 128, kernel_size=2)  # 64x22x22 -> 128x21x21
+        active3 = nn.ELU()
+        pool3 = nn.MaxPool2d(2)                    # 128x21x21 -> 128x10x10
+        drop3 = nn.Dropout(0.3)
+        
+        conv4 = nn.Conv2d(128, 256, kernel_size=1) # 128x10x10 -> 256x10x10
+        active4 = nn.ELU()
+        pool4 = nn.MaxPool2d(2)                    # 256x10x10 -> 256x5x5
+        drop4 = nn.Dropout(0.4)
+
+        flat = nn.Flatten()                        # 256x5x5 -> 6400
+
+        fc1 = nn.Linear(6400, 1000)                # 6400 -> 1000
+        active5 = nn.ELU()
+        drop5 = nn.Dropout(0.5)
+
+        fc2 = nn.Linear(1000, 1000)                # 1000 -> 1000
+        active6 = nn.ReLU()
+        drop6 = nn.Dropout(0.6)
+
+        fc3 = nn.Linear(1000, 30)                  # 1000 -> 30
+
+        self.layers = nn.Sequential(
+            conv1, active1, pool1, drop1,
+            conv2, active2, pool2, drop2,
+            conv3, active3, pool3, drop3,
+            conv4, active4, pool4, drop4,
+            flat,
+            fc1, active5, drop5,
+            fc2, active6, drop6,
+            fc3
+        )
         #######################################################################
         #                             END OF YOUR CODE                        #
         #######################################################################
 
     def forward(self, x):
         #######################################################################
-        # TODO: Define the feedforward behavior of this model                 #
+        # DONE: Define the feedforward behavior of this model                 #
         # x is the input image and, as an example, here you may choose to     #
         # include a pool/conv step:                                           #
         # x = self.pool(F.relu(self.conv1(x)))                                #
         # a modified x, having gone through all the layers of your model,     #
         # should be returned                                                  #
         #######################################################################
+
+        x = self.layers(x)
 
         #######################################################################
         #                           END OF YOUR CODE                          #
