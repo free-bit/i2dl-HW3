a241898fec0324f93141e8fae8e546c9f5ff8e02
free-bit <erkin.turkoz@metu.edu.tr>
2020-01-11 15:13:52 +0100

    Simple RNN implemented

--- exercise_code/rnn/rnn_nn.py
+++ exercise_code/rnn/rnn_nn.py
@@ -1,6 +1,10 @@
 import torch
 import torch.nn as nn
 
+act_layer = {
+    "relu": nn.ReLU,
+    "tanh": nn.Tanh
+}
 
 class RNN(nn.Module):
     def __init__(self, input_size=1, hidden_size=20, activation="tanh"):
@@ -12,12 +16,16 @@ class RNN(nn.Module):
         - activation: Nonlinearity in cell; 'tanh' or 'relu'
         """
         #######################################################################
-        # TODO: Build a simple one layer RNN with an activation with the      #
+        # DONE: Build a simple one layer RNN with an activation with the      #
         # attributes defined above and a forward function below. Use the      #
         # nn.Linear() function as your linear layers.                         #
-        # Initialse h as 0 if these values are not given.                     #
+        # Initialize h as 0 if these values are not given.                    #
         #######################################################################
-
+        self.hidden_size = hidden_size
+        self.ifc = nn.Linear(input_size, hidden_size)
+        self.hfc = nn.Linear(hidden_size, hidden_size)
+        self.active = act_layer[activation]()
+        print(self.active)
         #######################################################################
         #                           END OF YOUR CODE                          #
         #######################################################################
@@ -37,7 +45,18 @@ class RNN(nn.Module):
         #######################################################################
         #                                YOUR CODE                            #
         #######################################################################
+        h_seq = torch.Tensor()
+        seq_len, batch_size, input_size = x.shape
+        hidden_size = self.hidden_size
 
+        if not h:
+            h = torch.zeros(1, batch_size, hidden_size)
+        
+        for i in range(seq_len):
+            h_prev = self.hfc(h)                     # Take h_prev (h of previous layer) and feed it into the layer
+            h_now = self.ifc(x[i].unsqueeze_(0))     # Calculate h_now for the current input
+            h = self.active(h_prev + h_now)          # Calculate h_next
+            h_seq = torch.cat((h_seq, h), dim=0)     # Store it in a tensor
         #######################################################################
         #                           END OF YOUR CODE                          #
         #######################################################################
