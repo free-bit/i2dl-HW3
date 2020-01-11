67a5d8f27895659ddd4e98fa14da53e329137952
free-bit <erkin.turkoz@metu.edu.tr>
2020-01-11 20:25:14 +0100

    Changes: * LSTM, RNN_Classifier & LSTM_Classifier implemented * Up-to-date notebooks added

--- exercise_code/rnn/rnn_nn.py
+++ exercise_code/rnn/rnn_nn.py
@@ -25,7 +25,6 @@ class RNN(nn.Module):
         self.ifc = nn.Linear(input_size, hidden_size)
         self.hfc = nn.Linear(hidden_size, hidden_size)
         self.active = act_layer[activation]()
-        print(self.active)
         #######################################################################
         #                           END OF YOUR CODE                          #
         #######################################################################
@@ -45,18 +44,20 @@ class RNN(nn.Module):
         #######################################################################
         #                                YOUR CODE                            #
         #######################################################################
-        h_seq = torch.Tensor()
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        h_seq = torch.Tensor().to(device)
         seq_len, batch_size, input_size = x.shape
         hidden_size = self.hidden_size
 
+        # Initialize if not given
         if not h:
-            h = torch.zeros(1, batch_size, hidden_size)
+            h = torch.zeros(1, batch_size, hidden_size).to(device)
         
         for i in range(seq_len):
             h_prev = self.hfc(h)                     # Take h_prev (h of previous layer) and feed it into the layer
             h_now = self.ifc(x[i].unsqueeze_(0))     # Calculate h_now for the current input
             h = self.active(h_prev + h_now)          # Calculate h_next
-            h_seq = torch.cat((h_seq, h), dim=0)     # Store it in a tensor
+            h_seq = torch.cat((h_seq, h), dim=0)     # Store it in a tensor (vstack)
         #######################################################################
         #                           END OF YOUR CODE                          #
         #######################################################################
@@ -67,12 +68,28 @@ class LSTM(nn.Module):
     def __init__(self, input_size=1, hidden_size=20):
         super().__init__()
         #######################################################################
-        # TODO: Build a one layer LSTM with an activation with the attributes #
+        # DONE: Build a one layer LSTM with an activation with the attributes #
         # defined above and a forward function below. Use the                 #
         # nn.Linear() function as your linear layers.                         #
         # Initialse h and c as 0 if these values are not given.               #
         #######################################################################
+        self.hidden_size = hidden_size
 
+        self.hffc = nn.Linear(hidden_size, hidden_size) # hidden forget-gate FC (see f_t formula)
+        self.iffc = nn.Linear(input_size, hidden_size)  # input forget-gate FC (see f_t formula)
+        
+        self.hifc = nn.Linear(hidden_size, hidden_size) # hidden input-gate FC (see i_t formula)
+        self.iifc = nn.Linear(input_size, hidden_size)  # input input-gate FC (see i_t formula)
+        
+        self.hofc = nn.Linear(hidden_size, hidden_size) # hidden output-gate FC (see o_t formula)
+        self.iofc = nn.Linear(input_size, hidden_size)  # input output-gate FC (see o_t formula)
+        
+        self.hcfc = nn.Linear(hidden_size, hidden_size) # (see c_t formula)
+        self.icfc = nn.Linear(input_size, hidden_size)  # (see c_t formula)
+        
+        self.gsig = nn.Sigmoid()
+        self.csig = nn.Tanh()
+        self.hsig = nn.Tanh()
         #######################################################################
         #                           END OF YOUR CODE                          #
         #######################################################################
@@ -94,6 +111,44 @@ class LSTM(nn.Module):
         #######################################################################
         #                                YOUR CODE                            #
         #######################################################################
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        h_seq = torch.Tensor().to(device)
+        seq_len, batch_size, input_size = x.shape
+        hidden_size = self.hidden_size
+
+        # Initialize if not given
+        if not h:
+            h = torch.zeros(1, batch_size, hidden_size).to(device)
+        if not c:
+            c = torch.zeros(1, batch_size, hidden_size).to(device)
+
+        for i in range(seq_len):
+            c_prev = c
+            inp = x[i].unsqueeze_(0) # Add extra dimension to the beginning
+
+            # f_t
+            fh_prev = self.hffc(h)
+            fi = self.iffc(inp)
+            f_now = self.gsig(fi + fh_prev)
+
+            # i_t
+            ih_prev = self.hifc(h)
+            ii = self.iifc(inp)
+            i_now = self.gsig(ii + ih_prev)
+
+            # o_t
+            oh_prev = self.hofc(h)
+            oi = self.iofc(inp)
+            o_now = self.gsig(oi + oh_prev)
+
+            # c_t
+            ch_prev = self.hcfc(h)
+            ci = self.icfc(inp)
+            c = f_now * c_prev + i_now * self.csig(ci + ch_prev)
+
+            # h_t
+            h = o_now * self.hsig(c)
+            h_seq = torch.cat((h_seq, h), dim=0) # vstack
 
         #######################################################################
         #                           END OF YOUR CODE                          #
@@ -106,12 +161,19 @@ class RNN_Classifier(torch.nn.Module):
                  activation="relu"):
         super(RNN_Classifier, self).__init__()
         #######################################################################
-        #  TODO: Build a RNN classifier                                       #
+        #  DONE: Build a RNN classifier                                       #
         #######################################################################
-
+        self.rnn = RNN(input_size, hidden_size)
+        self.fc = nn.Linear(hidden_size, classes)
         #######################################################################
         #                           END OF YOUR CODE                          #
         #######################################################################
+
+    def forward(self, x):
+        _, x = self.rnn(x)
+        x = self.fc(x)
+        x.squeeze_(0)
+        return x
 
     def save(self, path):
         """
@@ -129,12 +191,19 @@ class LSTM_Classifier(torch.nn.Module):
     def __init__(self, classes=10, input_size=28, hidden_size=128):
         super(LSTM_Classifier, self).__init__()
         #######################################################################
-        #  TODO: Build a LSTM classifier                                      #
+        #  DONE: Build a LSTM classifier                                      #
         #######################################################################
-
+        self.lstm = LSTM(input_size, hidden_size)
+        self.fc = nn.Linear(hidden_size, classes)
         #######################################################################
         #                          END OF YOUR CODE                           #
         #######################################################################
+
+    def forward(self, x):
+        _, (x, _) = self.lstm(x)
+        x = self.fc(x)
+        x.squeeze_(0)
+        return x
 
     def save(self, path):
         """
